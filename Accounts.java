c16b888b10aa9979d220fa7ea4c70616a4a601cd
muhammad Umer Zia <55735751+umerzia-7001@users.noreply.github.com>
2020-06-11 22:13:25 +0500

    Account

---
+++ Accounts.java
@@ -0,0 +1,16 @@
+package java123;
+
+public interface Accounts {
+
+	// interface with methods to be implemented in childclass
+			
+	public Object withdraw(double amount);
+		
+	
+	public void depositBalance(double Balance);
+		
+	
+	public Object DisplayBalance();
+		
+	
+}
